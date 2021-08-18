f227c9aa253ce7e5e1cf819e3b571918f17a0709
Mateusz Boryn <mateusz.boryn@gmail.com>
2021-08-18 14:18:44 +0200

    bucket notifications from vars

--- tf/s3_bucket_notifications.tf
+++ tf/s3_bucket_notifications.tf
@@ -19,5 +19,102 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = true
 }
 
+resource "aws_sns_topic" "s3_notify" {
+  name = "zzz-s3-notify"
 
+    policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": { "Service": "s3.amazonaws.com" },
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:zzz-s3-notify",
+        "Condition":{
+            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.test_bucket.arn}"}
+        }
+    }]
+}
+POLICY
+}
+
+resource "aws_sns_topic" "s3_notify2" {
+  name = "zzz-s3-notify2"
+
+    policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": { "Service": "s3.amazonaws.com" },
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:zzz-s3-notify2",
+        "Condition":{
+            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.test_bucket.arn}"}
+        }
+    }]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_notification" "test_bucket_notify" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+/*
+  // having multiple "topic" entries here works well.
+  topic {
+    topic_arn     = aws_sns_topic.s3_notify.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "dir-a"
+  }
+  topic {
+    topic_arn     = aws_sns_topic.s3_notify.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "dir-b"
+  }
+  topic {
+    topic_arn     = aws_sns_topic.s3_notify.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "dir-c-2"
+  }
+  topic {
+    topic_arn     = aws_sns_topic.s3_notify.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "dir-d"
+  }
+*/
+
+  /*
+  // this approach keeps overwriting one entry with another
+  for_each = var.s3_notifications_list
+  topic {
+    topic_arn     = each.value.topic
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = each.value.prefix
+  }
+  */
+
+
+/*
+  // using for_each inside "topic" appears to be prohibited
+  topic {
+    for_each = var.s3_notifications_list
+
+    topic_arn     = each.value.topic
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = each.value.prefix
+  }
+  */
+
+
+  // this works as expected: create multiple notifications entries for a single bucket from a list or map
+  dynamic "topic" {
+    for_each = var.s3_notifications_list
+    content {
+      topic_arn     = topic.value.topic
+      events        = ["s3:ObjectCreated:*"]
+      filter_prefix = topic.value.prefix
+    }
+  }
+}
 
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,3 +1,17 @@
 variable "env" {default="dev"}
 variable "app_long" {default="TFSandbox"}
 variable "app" {default="TFSandbox"}
+variable "s3_notifications_list" {
+  default = {
+    "dira" = {
+      "topic" = "arn:aws:sns:eu-west-1:541147820420:zzz-s3-notify"
+      "prefix" = "dir-a-2"
+    }
+    "dirb" = {
+      "topic" = "arn:aws:sns:eu-west-1:541147820420:zzz-s3-notify"
+      "prefix" = "dir-b-2"
+    }
+
+    }
+
+}
